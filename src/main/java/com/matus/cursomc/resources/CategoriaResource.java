e91a366f6ef3a7176086af8d846083f15da6d3f6
UnDer-7 <mateus7532@gmail.com>
2018-06-30 00:40:46 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/matus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matus/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.matus.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matus.cursomc.domain.Categoria;
 
 @RestController // Criou umas classe Rest
 @RequestMapping(value="/categorias")// Vai responder por esse end-point: /categirias
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET) //se for colocar algo POST, deletar DELETE, etc
-	public String listar() {
-		return "Rest esta funcinando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
