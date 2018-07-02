3dec66cf01450c32a6948a44eb432175833d5ab8
“mateus.cardoso” <mateus.cardoso@basis.com.br>
2018-07-02 16:20:59 -0300

    (ARRUMAR); Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/matus/cursomc/CursomcApplication.java
+++ src/main/java/com/matus/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
-package com.matus.cursomc;
+	package com.matus.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.matus.cursomc.Repositorys.CidadeRepository;
 import com.matus.cursomc.Repositorys.ClienteRepository;
 import com.matus.cursomc.Repositorys.EnderecoRepository;
 import com.matus.cursomc.Repositorys.EstadoRepository;
+import com.matus.cursomc.Repositorys.PagamentoRepository;
+import com.matus.cursomc.Repositorys.PedidoRepository;
 import com.matus.cursomc.Repositorys.ProdutoRepository;
 import com.matus.cursomc.domain.Categoria;
 import com.matus.cursomc.domain.Cidade;
 import com.matus.cursomc.domain.Cliente;
 import com.matus.cursomc.domain.Endereco;
 import com.matus.cursomc.domain.Estado;
+import com.matus.cursomc.domain.Pagamento;
+import com.matus.cursomc.domain.PagamentoComBoleto;
+import com.matus.cursomc.domain.PagamentoComCartao;
+import com.matus.cursomc.domain.Pedido;
 import com.matus.cursomc.domain.Produto;
+import com.matus.cursomc.domain.enums.EstadoPagamento;
 import com.matus.cursomc.domain.enums.TipoCliente;
 
 @SpringBootApplication
@@ -37,6 +45,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository cliRepo;
 	@Autowired
 	private EnderecoRepository endRepo;
+	@Autowired
+	private PedidoRepository pedRepo;
+	@Autowired
+	private PagamentoRepository pagRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -88,5 +100,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		cliRepo.saveAll(Arrays.asList(cli1));
 		endRepo.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedRepo.saveAll(Arrays.asList(ped1, ped2));
+		pagRepo.saveAll(Arrays.asList(pagto1, pagto2));
+		
 	}
 }
