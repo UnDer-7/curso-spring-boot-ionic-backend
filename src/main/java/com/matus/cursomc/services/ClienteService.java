f75175e66c913d084dfc8ec83f6f8cbdb6763957
“mateus.cardoso” <mateus.cardoso@basis.com.br>
2018-07-02 13:51:25 -0300

    Criado endpoin /clientes/{id} disponivel

---
+++ src/main/java/com/matus/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.matus.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matus.cursomc.Repositorys.ClienteRepository;
+import com.matus.cursomc.domain.Cliente;
+import com.matus.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	//---------A classe SERVICE vai acessar o classe de REPOSITORY (private ClienteRepository repo;)---------
+	@Autowired// repo é automaticamente instaciando pelo Spring
+	private ClienteRepository repo;
+	
+	//Criar uma operacao q busca uma categoria por codigo
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: "+id+", Tipo: "+ Cliente.class.getName()));
+	}
+}
