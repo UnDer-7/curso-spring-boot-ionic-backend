4ea4a44ac7b4e82916bff8d6d43e3e864102de1b
“mateus.cardoso” <mateus.cardoso@basis.com.br>
2018-07-04 13:12:36 -0300

    Inserindo uma nova Categoria com Post (Fazendo um Insert)

--- src/main/java/com/matus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matus/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.matus.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matus.cursomc.domain.Categoria;
 import com.matus.cursomc.services.CategoriaService;
@@ -25,6 +29,16 @@ public class CategoriaResource {
 	// @PathVariable ==> Fala q o Integer id vai ser id da URL (value="/{id}") 
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj);	
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	// @RequestBody ==> Vai criar o Objeto apartir do JSon.
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		// uri ==> Vai criar a url pra acessar a categora
+		// fromCurrentRequest() ==> Pega a url usada como padrao, no caso: http://localhost:8081/pedidos/
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
