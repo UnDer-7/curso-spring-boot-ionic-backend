7d3ad0f6f334c008f9a9bf3f5a5970551db43ab4
UnDer-7 <mateus7532@gmail.com>
2018-07-08 17:50:00 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/matus/cursomc/resources/ClienteResource.java
+++ src/main/java/com/matus/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.matus.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matus.cursomc.domain.Cliente;
+import com.matus.cursomc.dto.ClienteDTO;
 import com.matus.cursomc.services.ClienteService;
 
 @RestController // Criou umas classe Rest
@@ -26,5 +35,40 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	//Update tem q pegar o obj pelo @RequestBody, e depois inserir o dado no obj pelo @PathVariable
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//Busca todos as categorias
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList()); // map() ==> Vai fazer uma opecao para cada elemento da lista
+		return ResponseEntity.ok().body(listDTO);	
+	}
+
+	@RequestMapping(value = "/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+	//@RequestParam ==> Para q vire opcional na URL, ex: categorias/page?page=0&linesPerPage = 20 etc
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = service.findPage(page,linesPerPage,orderBy,direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);	
 	}
 }
--- src/main/java/com/matus/cursomc/services/ClienteService.java
+++ src/main/java/com/matus/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.matus.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matus.cursomc.domain.Cliente;
+import com.matus.cursomc.dto.ClienteDTO;
 import com.matus.cursomc.repositorys.ClienteRepository;
+import com.matus.cursomc.services.exception.DataIntegrityException;
 import com.matus.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,43 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: "+id+", Tipo: "+ Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		// newObj vai buscar todos os dados no BD
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		//Quando o ID é null ele insere, quando ele existir ele atualiza o obj
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é posivel excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	// Vai pegar os dados por parte, ex: de 20 em 20, para diminuir o uso do banco
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	// Apartir de um obj categoria vai contruir um obj DTO
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		// Os dados no BD foi atualizado com os novos dados q foram passados no obj
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
