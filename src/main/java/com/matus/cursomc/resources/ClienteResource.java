4405d974f5ec06fc6214f96db176372ae7833dde
“mateus.cardoso” <mateus.cardoso@basis.com.br>
2018-08-31 12:23:58 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/matus/cursomc/resources/ClienteResource.java
+++ src/main/java/com/matus/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,7 +58,8 @@ public class ClienteResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
@@ -65,6 +67,7 @@ public class ClienteResource {
 	}
 	
 	//Busca todos as categorias
+    @PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
@@ -72,6 +75,7 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDTO);	
 	}
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 	//@RequestParam ==> Para q vire opcional na URL, ex: categorias/page?page=0&linesPerPage = 20 etc
