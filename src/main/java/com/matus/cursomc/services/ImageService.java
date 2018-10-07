b0dfb0811043cef6b46823d058a962ed2f4bb433
UnDer7 <mateus7532@gmail.com>
2018-10-07 15:47:13 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/matus/cursomc/services/ImageService.java
+++ src/main/java/com/matus/cursomc/services/ImageService.java
@@ -1,7 +1,9 @@
 package com.matus.cursomc.services;
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.matus.cursomc.services.exception.FileException;
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +20,7 @@ public class ImageService {
 
     /**
      * Método que faz a leitura da imagen e verfica se a mesma é JPG
+     *
      * @param uploadedFile
      * @return Retorna a imgagen no formato JPG
      */
@@ -42,6 +45,7 @@ public class ImageService {
 
     /**
      * Método que faz a converção de Png para Jpg
+     *
      * @param img - BufferedImage
      * @return returna um BufferedImage em Jpg
      */
@@ -51,13 +55,39 @@ public class ImageService {
         return jpgImage;
     }
 
-    public InputStream getInputStream(BufferedImage img, String extension){
-        try{
+    public InputStream getInputStream(BufferedImage img, String extension) {
+        try {
             ByteArrayOutputStream os = new ByteArrayOutputStream();
             ImageIO.write(img, extension, os);
             return new ByteArrayInputStream(os.toByteArray());
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new FileException("Erro ao ler arquivo");
         }
+    }
+
+    /**
+     * Método que corta a imagem para fica quadrada
+     * @param sourceImage - BufferedImage
+     * @return Retorna a iamgem cortada.
+     */
+    public BufferedImage cropSquare(BufferedImage sourceImage) {
+        //Descobre qual é o minimo, se for retrato corta horizontal, paisagem corta vertical
+        int min = (sourceImage.getHeight() <= sourceImage.getWidth()) ? sourceImage.getHeight() : sourceImage.getWidth();
+        return Scalr.crop(
+                sourceImage,
+                (sourceImage.getWidth() / 2) - (min / 2),
+                (sourceImage.getHeight() / 2) - (min / 2),
+                min,
+                min);
+    }
+
+    /**
+     * Método responsavel por diminuir a imgagemm
+     * @param sourceImage - BufferedImage
+     * @param size - int
+     * @return Retorna a imagem redimensionada
+     */
+    public BufferedImage resize(BufferedImage sourceImage, int size){
+        return Scalr.resize(sourceImage, Scalr.Method.ULTRA_QUALITY, size);
     }
 }
